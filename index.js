0109fc681b0d4d7ecb453bca88e2fb9210e788da
holomdev <holom.dev@gmail.com>
2020-11-05 11:38:36 -0600

    Estableciendo un router para las peliculas

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const { config } = require('./config/index');
+const moviesApi = require('./routes/movies');
 
 app.use(express.json());
+
+//routes
+moviesApi(app);
 
 app.listen(config.port, function () {
   // eslint-disable-next-line no-console
