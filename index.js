c9cf3b41e54e79041171d00f46231acdaa82088c
holomdev <holom.dev@gmail.com>
2020-11-06 14:47:43 -0600

    404 notFound

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 app.use(express.json());
 
 //routes
 moviesApi(app);
+
+//404
+app.use(notFoundHandler);
 
 app.listen(config.port, function () {
   // eslint-disable-next-line no-console
